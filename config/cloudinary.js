5c712f341f433cbff20f16f3adf8c2b8e49071f7
kikoslootbox <tech.mergency@gmail.com>
2026-05-11 12:18:16 -0700

    Fixed Cloudinary env names

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -3,14 +3,14 @@ require("cloudinary").v2;
 
 cloudinary.config({
 
-  cloud_name:
-  process.env.CLOUDINARY_NAME,
+cloud_name:
+process.env.CLOUDINARY_CLOUD_NAME,
 
-  api_key:
-  process.env.CLOUDINARY_KEY,
+api_key:
+process.env.CLOUDINARY_API_KEY,
 
-  api_secret:
-  process.env.CLOUDINARY_SECRET
+api_secret:
+process.env.CLOUDINARY_API_SECRET
 
 });
 
