7bf999600c27884f4bbfebc752fca1f8e4ada813
kikoslootbox <tech.mergency@gmail.com>
2026-05-15 16:45:26 -0700

    updated admin panel for users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -572,3 +572,25 @@ async(req,res)=>{
 });
 
 module.exports = router;
+
+router.get("/", async(req,res)=>{
+
+  try{
+
+    const users =
+
+    await User.find()
+
+    .sort({createdAt:-1});
+
+    res.json(users);
+
+  }catch(err){
+
+    res.status(500).json({
+      message:"Error loading users"
+    });
+
+  }
+
+});
